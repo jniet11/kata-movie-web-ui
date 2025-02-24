8e140800fab2c794d6b529c7d5c5b6b1f88e5d08
Juan Sebastian Nieto Solano <108822850+jniet11@users.noreply.github.com>
2025-02-23 21:32:01 -0500

    se agregan tipos

---
+++ src/types/reserveInterface.ts
@@ -0,0 +1,23 @@
+export interface Movie {
+  id: number;
+  title: string;
+}
+
+export interface Room {
+  id: number;
+  name: string;
+  capacity: number;
+}
+
+export interface Reservation {
+  id: number;
+  customer_name: string;
+  doc_number: string;
+  email: string;
+  movie_id: number;
+  room_id: number;
+  show_time: string;
+  seats: string[];
+  movie_title: string;
+  room_name: string;
+}
